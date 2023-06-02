93d20213962bd0be905cb91c7a5503119f7aa62c
Faridun Mainaev <farid.mainaiev@gmail.com>
2023-06-02 21:44:05 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menuBlockPhones = document.querySelector('.menu-block__phones');
+const menuBlockIconPhone = document.querySelector('.menu-block__icon--phone');
+const form = document.querySelector('.form-field');
+
+if (window.innerWidth >= 1280) {
+  menuBlockIconPhone.addEventListener('mouseover', function() {
+    menuBlockPhones.style.opacity = '1';
+  });
+
+  menuBlockPhones.addEventListener('mouseleave', function() {
+    menuBlockPhones.style.opacity = '0';
+  });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
