acc479189e52a8ce1a2d290fb083492420fa665a
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-06-02 10:32:32 +0200

    create landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const iconMenu = page.querySelector('.top-bar__icons-menu');
+const closeMenu = page.querySelector('.menu__content-top-close');
+
+iconMenu.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  page.classList.toggle('page--menu-active');
+});
+
+closeMenu.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  page.classList.toggle('page--menu-active');
+});
+
+const sub = document.querySelector('.contacts');
+const form = sub.querySelector('.contact');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
