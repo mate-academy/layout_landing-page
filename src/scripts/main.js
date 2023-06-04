5e04407581b7baac82194b04e3887a9e752d95cb
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-06-04 15:51:31 +0300

    landing page done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.contacts__form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
