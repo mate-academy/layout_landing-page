d188bf70fdb7ab3def5a69cd36411773d077bd13
Pavlo Chudin <maks.chudin567@gmail.com>
2023-06-04 04:10:51 +0300

    did full blocks,burger-menu,last-step phone animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menu__toggle');
+  const menuLinks = document.querySelectorAll('.menu__box a');
+
+  menuToggle.addEventListener('change', function() {
+    if (!this.checked) {
+      return;
+    }
+
+    menuLinks.forEach(function(link) {
+      link.addEventListener('click', function() {
+        setTimeout(function() {
+          menuToggle.checked = false;
+        }, 300);
+      });
+    });
+  });
+});
