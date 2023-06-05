17500d73f7825b59d1614196377795255c1c2dab
Faridun Mainaev <farid.mainaiev@gmail.com>
2023-06-05 14:22:39 +0300

    five june commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,12 @@ const form = document.querySelector('.form-field');
 if (window.innerWidth >= 1280) {
   menuBlockIconPhone.addEventListener('mouseover', function() {
     menuBlockPhones.style.opacity = '1';
+    menuBlockPhones.style.pointerEvents = 'auto';
   });
 
   menuBlockPhones.addEventListener('mouseleave', function() {
     menuBlockPhones.style.opacity = '0';
+    menuBlockPhones.style.pointerEvents = 'none';
   });
 }
 
