6db232d618dd9b64a0d55d9a7c84df0b78427d35
Pavlo Chudin <maks.chudin567@gmail.com>
2023-06-06 17:48:50 +0300

    add logick for burger-menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,24 @@
 'use strict';
 
 const menuBtn = document.querySelector('.menu-btn');
-const burger = document.querySelector('.menu');
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu__item');
 
 menuBtn.addEventListener('click', function() {
   menuBtn.classList.toggle('active');
-  burger.classList.toggle('active');
+  menu.classList.toggle('active');
+
+  if (menu.classList.contains('active')) {
+    document.body.classList.add('menu-active');
+  } else {
+    document.body.classList.remove('menu-active');
+  }
+});
+
+menuLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    menuBtn.classList.remove('active');
+    menu.classList.remove('active');
+    document.body.classList.remove('menu-active');
+  });
 });
