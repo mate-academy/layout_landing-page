bf080446247c71cca57c736eec477fe62988d3a4
Alyona Sarapina <alyona.sarapina@gmail.com>
2023-06-07 11:59:08 +0100

    add dark theme switcher

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const logoLink = document.querySelector('.top-bar__logo-link');
+
+logoLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  page.classList.toggle('page--theme--dark');
+});
