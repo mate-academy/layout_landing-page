20d744700f0060c0581d50e780eab205b854570d
alex <cebanoleksandr@gmail.com>
2023-06-09 18:40:19 +0300

    fix mistakes again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let currentUrl = window.location.href;
+
+const page = document.querySelector(".page");
+
+document.addEventListener("click", () => {
+  currentUrl = window.location.href;
+  console.log(currentUrl);
+
+  if (currentUrl.includes("menu")) {
+    page.classList.remove("hide-scroll");
+  } else {
+    page.classList.add("hide-scroll");
+  }
+});
