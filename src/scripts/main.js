1bf57f78d43357b86830ca3b88e8224f1819b133
Pavlo Chudin <maks.chudin567@gmail.com>
2023-06-10 12:12:20 +0300

    did logick for logo burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuLinks = document.querySelectorAll('.menu__item');
 const form = document.querySelector('.contacts__form');
+const menuLink = document.querySelector('.menu__link');
+const homeLink = document.querySelector('.home-link');
 
 menuBtn.addEventListener('click', function() {
   menuBtn.classList.toggle('active');
@@ -27,4 +29,30 @@ menuLinks.forEach(function(link) {
     menu.classList.remove('active');
     document.body.classList.remove('menu-active');
   });
+});
+
+menuBtn.addEventListener('click', function() {
+  if (!menu.classList.contains('active')) {
+    menu.style.transform = 'translateX(100%)';
+  } else {
+    menu.style.transform = '';
+  }
+});
+
+function handleMenuLinkClick(event) {
+  event.preventDefault();
+  menu.style.transform = 'translateX(100%)';
+  menuBtn.classList.remove('active');
+  menu.classList.remove('active');
+  document.body.classList.remove('menu-active');
+}
+
+menuLink.addEventListener('click', handleMenuLinkClick);
+
+homeLink.addEventListener('click', function() {
+  if (menu.classList.contains('active')) {
+    menuBtn.classList.remove('active');
+    menu.classList.remove('active');
+    document.body.classList.remove('menu-active');
+  }
 });
