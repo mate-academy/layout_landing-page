93caccd0786f6167b25c112d7807196f6f5a9dbc
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-06-16 17:12:44 +0300

    Solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const btnOff = document.querySelector('.icon--menu');
+const btnOn = document.querySelector('.icon--close');
+const navLink = document.querySelector('.nav__list');
+
+const body = document.body;
+
+function disableScroll() {
+  body.classList.add('disable-scroll');
+};
+
+function enableScroll() {
+  body.classList.remove('disable-scroll');
+};
+
+btnOff.addEventListener('click', e => {
+  disableScroll();
+});
+
+btnOn.addEventListener('click', e => {
+  enableScroll();
+});
+
+navLink.addEventListener('click', e => {
+  enableScroll();
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  form.reset();
+});
