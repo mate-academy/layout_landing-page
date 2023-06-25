22ebb9151a20f3b2557d283f4d8cb32488ee7084
Daniel Hladkyi <gladkiy1518@gmail.com>
2023-06-25 13:01:21 +0300

    Finished, need to fix few bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const burgerMenu = document.querySelector('.header__burger-menu');
+const html = document.querySelector('html');
+const navLinkHeader = document.querySelectorAll('.nav__link');
+const form = document.querySelector('.contact-us__form');
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('header__burger-menu--active');
+  menu.classList.toggle('menu--active');
+  html.classList.toggle('block-scroll');
+});
+
+navLinkHeader.forEach((link) => {
+  link.addEventListener('click', () => {
+    burgerMenu.classList.toggle('header__burger-menu--active');
+    menu.classList.toggle('menu--active');
+    html.classList.toggle('block-scroll');
+  });
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
