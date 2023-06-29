5c0d9191d64e65d59668b72a8be2f5d5096c55b2
Ulcja <ulcja.ua@gmail.com>
2023-06-29 11:21:19 +0200

    29/06

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,14 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const send = document.querySelector('#button');
+
+send.addEventListener('click', checkboxClick, false);
+
+function checkboxClick(event) {
+  const warn = "preventDefault() won't let you check this!<br>";
+
+  document.getElementById('output-box').innerHTML += warn;
+  event.preventDefault();
+}
