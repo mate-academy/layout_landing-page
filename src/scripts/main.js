a6ee2c157b66d7b277601aa953a76b42eb0ead8d
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-07-01 16:46:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function initApp() {
+  const closeBtn = document.getElementById('close');
+  const openBtn = document.getElementById('open');
+  const nav = document.getElementById('menu');
+
+  openBtn.addEventListener('click', () => {
+    nav.classList.add('is-active');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    nav.classList.remove('is-active');
+  });
+}
+
+initApp();
