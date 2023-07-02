209a0cdcca46983235a357ba3eeb21e6f7441b3e
dmytronykorovych <dmytro.nykorovych@gmail.com>
2023-07-02 20:33:27 +0200

    Finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav') {
+    document.body.classList.add('page__body--with-nav');
+  } else {
+    document.body.classList.remove('page__body--with-nav');
+  }
+});
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.classList.add('header--scroll');
+  } else {
+    header.classList.remove('header--scroll');
+  }
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  form.reset();
+});
