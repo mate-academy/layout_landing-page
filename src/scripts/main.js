2935c87573e746414f07ac7ced57d62a65c8aa18
Nigga <yomzfk@gmail.com>
2023-07-16 13:20:17 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const html = document.querySelector('html');
+
+document.querySelector('.icon--burger').addEventListener('click', () => {
+  html.classList.toggle('unscroll');
+});
+
+document.querySelector('.icon--close').addEventListener('click', () => {
+  html.classList.toggle('unscroll');
+});
