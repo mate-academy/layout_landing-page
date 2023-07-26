0534d19726246238dc44d274fbb74358ca2e1757
AndrMar1939 <advo737@gmail.com>
2023-07-26 20:18:03 +0300

    first version of landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const body = document.querySelector('body');
+const openMenuButton = document.querySelector('#menu-open');
+const closeMenuButton = document.querySelector('#menu-close');
+const menu = document.querySelector('#page-menu');
+const menuNavLinks = document.querySelectorAll('.menu__link');
+
+const toClickOnMenu = () => {
+  menu.classList.toggle('page__menu--active');
+  body.classList.toggle('page__body--menu-active');
+};
+
+openMenuButton.addEventListener('click', toClickOnMenu);
+
+closeMenuButton.addEventListener('click', toClickOnMenu);
+
+const navLinks = Array.from(menuNavLinks);
+
+navLinks.forEach((item) => {
+  item.addEventListener('click', toClickOnMenu);
+});
