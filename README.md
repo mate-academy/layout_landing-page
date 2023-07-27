1a23aaadaff21b87795325820005a1fb445ea688
AndrMar1939 <advo737@gmail.com>
2023-07-27 14:28:42 +0300

    add observer and appearance animation to elements; add hover to img; fix font weight

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Landing page
 [DEMO LINK](https://AndrMar1939.github.io/layout_landing-page/)
+[MOCKUP LINK](https://www.figma.com/file/lSR1m42L9YwzQwzzxKwHpw/THE-MET)
+
+A landing for The metropolitan museum of art (THE MET);
+
+- a main layout made with Flexbox;
+- appearance of elements made with IntersectionObserver API;
+- a menu made with onClick function;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,7 @@ const closeMenuButton = document.querySelector('#menu-close');
 const menu = document.querySelector('#page-menu');
 const menuNavLinks = document.querySelectorAll('.menu__link');
 
+// clicks
 const toClickOnMenu = () => {
   menu.classList.toggle('page__menu--active');
   body.classList.toggle('page__body--menu-active');
@@ -20,3 +21,20 @@ const navLinks = Array.from(menuNavLinks);
 navLinks.forEach((item) => {
   item.addEventListener('click', toClickOnMenu);
 });
+
+// animation with scroll
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+
+const options = { threshold: [1] };
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.element-with-animation');
+
+for (const element of elements) {
+  observer.observe(element);
+}
