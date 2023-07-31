3a56ee6790438ee896faebc3982b37d6b87f09d3
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-07-31 19:04:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,11 @@ function initApp() {
   const openBtn = document.getElementById('open');
   const links = document.querySelectorAll('.nav__link');
   const nav = document.getElementById('menu');
+  const navLinks = document.querySelectorAll('header nav a');
+
+  for (const link of navLinks) {
+    link.addEventListener('click', smoothScroll);
+  }
 
   openBtn.addEventListener('click', () => {
     nav.classList.add('is-active');
@@ -25,3 +30,15 @@ function initApp() {
 }
 
 initApp();
+
+function smoothScroll(event) {
+  event.preventDefault();
+
+  const href = this.getAttribute('href');
+
+  document.querySelector(href).scrollIntoView({
+
+    behavior: 'smooth',
+
+  });
+}
