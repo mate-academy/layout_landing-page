46d7fe12cf7a389d3165d2ecbc3988304aed7425
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-08-06 13:30:41 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function toggleModal() {
+  const modal = document.getElementById('modalMenu');
+
+  modal.classList.toggle('show');
+}
+
+function closeModal() {
+  const modal = document.getElementById('modalMenu');
+
+  modal.classList.remove('show');
+}
