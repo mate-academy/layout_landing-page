feb7b8753a3600454984dee3a2e6ec4cd92b9840
Roman Harasymchuk <hrs2511@gmail.com>
2023-08-14 12:40:38 -0400

    miami changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,1 @@
-function submitForm(event) {
-  // Validate the form data.
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-
-  if (name === "" || email === "") {
-    alert("Please enter your name and email address.");
-    event.preventDefault();
-    return;
-  }
-
-  // Submit the form.
-  document.getElementById("myForm").submit();
-}
-
-// Bind the onsubmit event to the form.
-document.getElementById("myForm").addEventListener("submit", submitForm);
+'use strict';
