69e7cbed01266d71452b43a1796b3dd28deee5f8
Mykhailo Shevchuk <mishashevchuk@gmail.com>
2023-08-20 19:58:00 +0300

    requested changes were made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,16 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-const button = document.querySelector('#submit');
-const form = document.querySelector('.form');
-
-button.addEventListener('click', function(event) {
-  event.preventDefault();
-
-  const result = form.reportValidity();
-
-  if (result === true) {
-    form.reset();
-  }
-}, false);
