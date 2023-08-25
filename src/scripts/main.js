17bcbe2944420dc0be7c030e2276cfe7ed53ff41
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2023-08-25 13:20:02 +0100

    landing-page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const triggerElement = document.querySelector('.icon--call');
+const targetElement = document.querySelector('.top-bar__hover-desktop');
+
+triggerElement.addEventListener('mouseenter', () => {
+  targetElement.classList.add('top-bar__hover-desktop--active');
+});
+
+triggerElement.addEventListener('mouseleave', () => {
+  targetElement.classList.remove('top-bar__hover-desktop--active');
+});
+
+targetElement.addEventListener('mouseenter', () => {
+  targetElement.classList.add('top-bar__hover-desktop--active');
+});
+
+targetElement.addEventListener('mouseleave', () => {
+  targetElement.classList.remove('top-bar__hover-desktop--active');
+});
