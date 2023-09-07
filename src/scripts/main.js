200e9a64588ff836d706bd9bdc890a8ecb481d33
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-09-07 13:06:19 +0300

    added animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('animated-block-show');
+    }
+  });
+}
+
+const options = { threshold: [0.7] };
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.animated-block');
+
+for (const elm of elements) {
+  observer.observe(elm);
+}
