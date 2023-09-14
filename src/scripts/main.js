349b40708dd83cf08c43c1267b4737aebd09a962
Yaroslav <slavik2607.lviv@gmail.com>
2023-09-14 10:18:04 +0300

    landing page without animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css'; // You can also use <link> for styles
+// // ..
+// AOS.init();
+
+// AOS.init({
+//   disable: false,
+//   startEvent: 'DOMContentLoaded',
+//   initClassName: 'aos-init',
+//   animatedClassName: 'aos-animate',
+//   useClassNames: false,
+//   disableMutationObserver: false,
+//   debounceDelay: 50,
+//   throttleDelay: 99,
+//   offset: 120,
+//   delay: 0,
+//   duration: 400,
+//   easing: 'ease',
+//   once: false,
+//   mirror: false,
+//   anchorPlacement: 'top-bottom',
+// });
