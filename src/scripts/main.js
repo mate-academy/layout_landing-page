3dd0aba7b31bb800eccadf63b821031302139085
Kirill <ovoo1339@gmail.com>
2023-09-14 16:33:46 +0300

    MyBike

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const phoneIcon = document.querySelector('.header__icon--phone');
+  const phoneNumber = document.querySelector('.header__number');
+
+  phoneIcon.addEventListener('mouseover', function() {
+    phoneNumber.style.display = 'block';
+  });
+
+  phoneIcon.addEventListener('mouseout', function() {
+    phoneNumber.style.display = 'none';
+  });
+});
+
+document.getElementById('openMaps').addEventListener('click', function() {
+  // eslint-disable-next-line max-len
+  window.open('https://www.google.com/maps/place/Limoges,+Prancis/@45.8584921,1.1494858,12z/data=!4m6!3m5!1s0x47f934afe6680461:0x10be4a3159364d11!8m2!3d45.833619!4d1.261105!16zL20vMDE4MHls?entry=ttu', '_blank');
+});
