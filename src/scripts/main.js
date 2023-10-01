ed1fdd560ca85dccafe154c6d387000039571b1b
boytrend0108 <boytrend@gmail.com>
2023-10-01 10:17:02 +0300

    fix contact section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const body = document.querySelector('.page__content');
+
+window.addEventListener('scroll', addPointerEventsForFooter);
+
+function addPointerEventsForFooter() {
+  if (
+    document.documentElement.scrollTop > (document.documentElement.scrollHeight * 0.7)
+  ) {
+    body.style.pointerEvents = 'none';
+    console.log('true');
+  } else {
+    body.style.pointerEvents = 'all';
+    console.log('false');
+  }
+}
