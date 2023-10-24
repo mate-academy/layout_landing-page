ac481810537be39bbc0a540a066072983cdbcf4a
“Julia <j.shkorupatska@gmail.com>
2023-10-24 16:23:31 +0300

    add phone hover in header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const modalOpen = document.getElementById('openModal');
+const modal = document.getElementById('modal');
+const modalClose = document.getElementById('modalClose');
+
+modalOpen.onclick = function() {
+  if (window.screen.width < 1260) {
+    modal.style.display = 'block';
+  }
+};
+
+modalClose.onclick = function() {
+  modal.style.display = 'none';
+};
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
