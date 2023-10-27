64552f1050e75031edabf5b7921b994841761f92
Mykola Plekan <zikzar@gmail.com>
2023-10-27 16:32:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const form = document.getElementById('myForm');
+const nameInput = document.querySelector('input[name="name"]');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  nameInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
