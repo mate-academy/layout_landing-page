a010ce779fbcbbe051ff0aad2f6461582ae935e1
Serhii <gashysh@protonmail.com>
2023-10-29 00:36:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const form = document.querySelector('[name="contact-form"]');
+
+window.addEventListener('hashchange', () => {
+  if (
+    window.location.hash === '#menu'
+    || window.location.hash === '#all-products'
+  ) {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
