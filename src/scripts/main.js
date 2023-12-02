bb2679b225e96b64a01a47cdcd1c35fda521e0b7
Mariana <marjanavv@gmail.com>
2023-12-02 09:17:03 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// const iconMenuEl = document.querySelector('.icon--menu');
+
+// iconMenuEl.addEventListener('click', () => {
+//   const bodyEl = document.querySelector('body');
+
+//   if (bodyEl.classList.contains('over')) {
+//     bodyEl.classList.remove('over');
+//   } else {
+//     bodyEl.classList.add('over');
+//   }
+// });
+
+// .over {
+//   overflow: hidden;
+// }
