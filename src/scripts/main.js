0ff74a8fc256311f5066cbb9595717c1f0ade8ed
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-03 18:27:34 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const blocks = document.querySelectorAll('.page__section');
+  const headerTitle = document.querySelector('.header__title');
+  let firstLoad = true;
+
+  function handleScroll() {
+    if (firstLoad) {
+      headerTitle.style.transform = 'translateX(0)';
+      firstLoad = false;
+    } else {
+      blocks.forEach((block) => {
+        const blockTop = block.getBoundingClientRect().top;
+
+        if (blockTop < window.innerHeight - 100) {
+          block.style.opacity = '1';
+          block.style.transform = 'translateY(0)';
+        }
+      });
+    }
+  }
+
+  // Initial check in case some blocks are already in the viewport
+  handleScroll();
+
+  // Listen for scroll events
+  window.addEventListener('scroll', handleScroll);
+});
