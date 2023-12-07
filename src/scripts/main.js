ab4780a2ff585f3108459b907fa427c783a6c0d3
Yaroslav Samoilenko <yasik2255@live.com>
2023-12-07 12:20:08 +0200

    added fixes: fonts\\cursor\colors moved to variables, new structure of files, js for shader height

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const topBar = document.getElementById('top-bar');
+const pageShader = document.getElementById('page-shader');
+
+window.onresize = setShaderHeight;
+
+setShaderHeight();
+
+function setShaderHeight() {
+  pageShader.setAttribute('style', `height: ${topBar.offsetHeight}px;`);
+};
