616445578243210b0944215fe158890490fcbe35
Burchak Dmitry <burchak.dmitry@icloud.com>
2023-12-09 15:54:53 +0200

    removed the fixed width and added memory to the input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,7 @@ const phone = document.querySelector('.icon--phone');
 phone.onmouseover = function addClassActive() {
   number.classList.toggle('active');
 };
+
+phone.onmouseout = function addClassActive() {
+  number.classList.toggle('active');
+};
