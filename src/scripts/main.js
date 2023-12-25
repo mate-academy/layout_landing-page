98b9c17c045c3973c05b8160c20d4d7293a15bbe
Bilokon Yaroslav <uvill.yarik@gmail.com>
2023-12-25 20:54:57 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const menu = page.querySelector('.icon--menu');
+const closeMenu = page.querySelector('.icon--close');
+
+menu.addEventListener('click', () => {
+  page.classList.add('page--hidden');
+});
+
+closeMenu.addEventListener('click', () => {
+  page.classList.remove('page--hidden');
+});
