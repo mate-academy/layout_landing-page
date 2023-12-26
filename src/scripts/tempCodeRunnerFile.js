ab5e2800dbc06722587cef8fa42eb6a664ab5a35
beliyandr <beliyandr@gmail.com>
2023-12-26 22:14:38 +0200

    fix sample_1.2

--- src/scripts/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function printBackwards(input, start, end) {
-  for (let i = end; i >= start; i - 1) {
-    console.log(input[i]);
-  }
-}
-
-console.log(printBackwards("javassss", 2, 6));
