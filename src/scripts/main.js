1ac513be9b6aae8bb112bddc664938bd76d85086
Nila <nilamadhava05@gmail.com>
2023-12-30 22:12:38 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,1 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-
-hiddenElements.forEach((el) => observer.observe(el));
+'use strict';
