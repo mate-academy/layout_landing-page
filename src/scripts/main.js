779dcd0bc801fdda5f8ba105ce62986734da085b
Denis Reznik <reznik.d.o12@gmail.com>
2024-01-13 23:54:43 +0200

    add main code and animation blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-'use strict';
+import { animOnScroll } from './animOnScroll';
+
+const animItems = document.querySelectorAll('.js-anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+}
+
+setTimeout(() => {
+  animOnScroll();
+}, 300);
