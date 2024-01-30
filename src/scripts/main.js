bc0043f86b4854793a8983f27c5bc337b505ff04
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-01-30 15:54:21 +0100

    header, menu, recommended done with responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuButton = document.querySelector('.icon--menu');
+  const closeButton = document.querySelector('.icon--close');
+  const menu = document.querySelector('.menu');
+
+  menuButton.addEventListener('click', function() {
+    menu.style.display = 'flex';
+  });
+
+  closeButton.addEventListener('click', function() {
+    menu.style.display = 'none';
+  });
+});
