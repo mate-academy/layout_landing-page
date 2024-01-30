8e57ea433c88e3d80103deca427581b6f921ce53
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-01-30 17:45:46 +0200

    created header desktop version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello World!');
+  toglleMenu();
+});
+
+const toglleMenu = () => {
+  const menuToggle = document.querySelector('.header .header__buttons-toggle');
+  const body = document.querySelector('body');
+  const menuNav = document.querySelector('#menu');
+
+  if (!menuToggle) {
+    return;
+  }
+
+  menuToggle.addEventListener('click', el => {
+    body.classList.toggle('body--fixed');
+    menuNav.classList.toggle('menu--active');
+    menuToggle.classList.toggle('header__buttons-toggle--active');
+  });
+};
