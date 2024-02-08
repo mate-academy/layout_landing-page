601b41b46cb41d973a7a9c7d303a0a60d196953f
Your Name <kykyangelina182@gmail.com>
2024-02-08 19:09:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burgerIcon = document.querySelector('.burgerIcon');
+const burgerMenu = document.querySelector('.burgerMenu');
+// const burgerMenuCloseIcon = document.querySelector('.burgerMenuCloseIcon');
+
+burgerIcon.addEventListener('click', () => {
+  burgerMenu.classList.toggle('open');
+  burgerIcon.classList.toggle('close');
+});
+
+// burgerMenuCloseIcon.addEventListener('click', () => {
+//   burgerMenu.classList.toggle('open');
+// });
