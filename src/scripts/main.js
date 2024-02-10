2099a33aaa11df62abbd02e359fb2560bdde463e
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-02-10 23:55:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const menuOpener = document.querySelector('.icon--menu');
+const menuCloser = document.querySelector('.icon--close');
+
+menuOpener.addEventListener('click', function() {
+  page.classList.add('no-scroll');
+});
+
+menuCloser.addEventListener('click', function() {
+  page.classList.remove('no-scroll');
+});
