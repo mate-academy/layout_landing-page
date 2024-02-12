87d97ae389593e680d9c5b3b415b0bf728f382a0
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-02-12 17:57:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const menuOpener = document.querySelector('.icon--menu');
+const menuCloser = document.querySelector('.icon--close');
+const menuLinks = document.querySelectorAll('.nav__link');
+const logos = document.querySelectorAll('.top-bar__logo');
+const menuLogo = logos[1];
+
+menuOpener.addEventListener('click', function() {
+  page.classList.add('no-scroll');
+});
+
+menuCloser.addEventListener('click', function() {
+  page.classList.remove('no-scroll');
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    page.classList.remove('no-scroll');
+  });
+});
+
+menuLogo.addEventListener('click', function() {
+  page.classList.remove('no-scroll');
+});
