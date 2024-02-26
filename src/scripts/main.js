6c4d42ea44eae83db360898fac3bb066a534f322
Yevhenii Kushnir <aroblack17@gmail.com>
2024-02-26 01:28:36 +0100

    completed second part

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+// (function () {
+//   let img = document.querySelector('.icon__burger-menu');
+
+//   img.addEventListener('mouseenter', function () {
+//     console.log(img);
+//     img.src = './scripts/icon-burger-menu-hover.svg';
+//   });
+//   img.addEventListener('mouseleave', function () {
+//     img.src = './scripts/icon-burger-menu.svg';
+//   });
+// })();
