bf57bb4e818827da29c3e2b7f68ccabadd508b5a
Marian <getmoodyup@gmail.com>
2024-02-29 22:45:41 +0200

    MyBike is built

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const pageBody = document.getElementById('page-body');
+const form = document.getElementById('form');
+
+function closeMenu() {
+  pageBody.classList.remove('page__body--overflow-hidden');
+  pageBody.classList.add('page__body--overflow-auto');
+}
+
+window.addEventListener('hashchange', function() {
+  if (this.location.hash === '#menu') {
+    pageBody.classList.add('page__body--overflow-hidden');
+  } else {
+    closeMenu();
+  }
+});
