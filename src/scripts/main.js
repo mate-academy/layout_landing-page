fd2e5226a697d3c44ef2bb2f5015d7d3c9617ca9
Erik Hulinian <gulinianerik2@gmail.com>
2024-03-01 19:56:54 +0200

    fixed landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const phoneIcon = document.getElementById('phone-icon');
+const phoneNumber = document.getElementById('phone-number');
+
+if (phoneIcon && phoneNumber) {
+  phoneNumber.style.display = 'none';
+
+  phoneIcon.addEventListener('mouseover', function() {
+    phoneNumber.style.display = 'block';
+  });
+
+  phoneIcon.addEventListener('mouseout', function() {
+    phoneNumber.style.display = 'none';
+  });
+}
