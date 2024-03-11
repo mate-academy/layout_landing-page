e27f2cfb5781554d886d982bbc48279d1f70ccc9
Your Name <kykyangelina182@gmail.com>
2024-03-11 11:31:53 +0000

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,23 @@ burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle('close');
 });
 
+const headerLinks = document.querySelectorAll('.burgerMenuBody a');
+
+headerLinks.forEach(el => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    burgerMenu.classList.toggle('open');
+    burgerIcon.classList.toggle('close');
+
+    const idTo = e.target.dataset.to;
+    const targetElement = document.getElementById(idTo);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
 // burgerMenuCloseIcon.addEventListener('click', () => {
 //   burgerMenu.classList.toggle('open');
 // });
