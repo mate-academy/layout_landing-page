9a2046ae59f3677ef5943d1f50a8dc867a3fc885
Andrii <andriykostiuk1@gmail.com>
2024-03-17 21:46:54 +0200

    fix scroll on menu, update links, wrap images to links, fix form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const contactsForm = document.querySelector('.contacts__form');
+
+contactsForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  contactsForm.reset();
+});
