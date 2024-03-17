2a05c9b4a2a7fb81329dc650e64079ae70499cc6
Andrii <andriykostiuk1@gmail.com>
2024-03-17 22:04:06 +0200

    set footer position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,1 @@
 'use strict';
-
-const contactsForm = document.querySelector('.contacts__form');
-
-contactsForm.addEventListener('click', (e) => {
-  e.preventDefault();
-  contactsForm.reset();
-});
