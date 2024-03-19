bc83b7659829fa676a71da94a8783e724e88f86d
Yaroslav Ivasenko <yarik.dock@gmail.com>
2024-03-19 15:36:34 +0200

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-'use strict';
+const menuBtn = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+const close = document.getElementById('close-btn');
+
+function toggleMenu() {
+  menu.classList.toggle('active');
+  document.body.classList.toggle('body-scroll-disabled');
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+
+close.addEventListener('click', function() {
+  menu.classList.remove('active');
+  document.body.classList.remove('body-scroll-disabled');
+});
