18ac5a0b1547bac906e1be9523504b076696609d
Daniil Manakov <daniilmanakov04@gmail.com>
2024-03-19 08:28:11 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var form = document.getElementById('contactForm');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Очищаємо поля вводу
+    var nameInput = document.getElementsByName('name')[0];
+    var emailInput = document.getElementsByName('email')[0];
+    var messageInput = document.getElementsByName('text')[0];
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
+
+  });
+});
