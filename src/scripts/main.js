c6db0acf99909420b6cf02451b7cfc17d0a4188b
Polina <polinabondar4@gmail.com>
2024-03-20 23:20:07 +0200

    update according to requirements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menuIcon = document.querySelector('.icon--menu');
+const closeIcon = document.querySelector('.icon--close');
+const body = document.body;
+
+menuIcon.addEventListener('click', function () {
+  body.classList.add('lock-scroll');
+});
+
+closeIcon.addEventListener('click', function () {
+  body.classList.remove('lock-scroll');
+});
