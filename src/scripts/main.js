59b592f0ebbc71469c39bb9c26e73bcc6101a0bb
Vladislav <7evenLays@gmail.com>
2024-03-22 16:14:43 +0200

    added pointer and fixed animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,9 @@
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) {
+    if (entry.isIntersecting && !entry.target.classList.contains('show')) {
       entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
+    } // else entry.target.classList.remove('show');
   });
 });
 
