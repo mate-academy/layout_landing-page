94b1952a10b385d5e80aea93a7fe16db945a6f6d
Vladislav <7evenLays@gmail.com>
2024-03-22 14:52:14 +0200

    done with landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,56 @@
 'use strict';
+// =================== Animations ==================================
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+const numberShow = document.querySelector('.icon--number');
+
+const numberButton = document.querySelector('.icon--phone');
+
+numberButton.addEventListener('mouseenter', () => {
+  numberShow.classList.add('icon--number-show');
+});
+
+numberButton.addEventListener('mouseleave', () => {
+  setTimeout(() => {
+    numberShow.classList.remove('icon--number-show');
+  }, 3000);
+});
+
+// =================== Scroll UP ==================================
+const scrollup = document.querySelector('.scrollup');
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+scrollup.addEventListener('click', () => {
+  setTimeout(() => {
+    scrollToTop();
+  }, 300);
+});
+
+// scrollup.addEventListener('click', scrollToTop);
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollup.style.visibility = 'visible';
+  } else {
+    scrollup.style.visibility = 'hidden';
+  }
+});
