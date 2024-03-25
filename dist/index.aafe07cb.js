d8ecb460b0820cbd6bb280f0b71a4b83d9713e34
Bohdan <bqkapko@gmail.com>
2024-03-25 15:02:35 +0200

    make build

---
+++ dist/index.aafe07cb.js
@@ -0,0 +1,3 @@
+document.getElementById("contact-us__form").addEventListener("submit",function(t){t.preventDefault(),this.reset()});//# sourceMappingURL=index.aafe07cb.js.map
+
+//# sourceMappingURL=index.aafe07cb.js.map
--- dist/index.f75de5e1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-document.getElementById("contact-us__form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    this.reset();
-});
-
-//# sourceMappingURL=index.f75de5e1.js.map
