771566ba7af7cc7f61030ef938c2aee338d7729a
Bohdan <bqkapko@gmail.com>
2024-03-25 15:42:53 +0200

    make build

---
+++ dist/index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+document.getElementById("contact-us__form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    this.reset();
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
