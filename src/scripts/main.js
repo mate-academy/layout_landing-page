02b0c51d947557d34b85bb53b2f537dc3a866b6e
Denys Lytvyn <ddl@lytvyn.com>
2024-04-08 20:05:07 +0300

    scroll and btn fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const body = document.querySelector('body');
+const menuIcon = document.querySelector('.icon--menu');
+const menuCloseIcon = document.querySelector('.icon--close');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', function() {
+  body.classList.add('no-scroll');
+  menu.classList.add('open');
+});
+
+menuCloseIcon.addEventListener('click', function() {
+  body.classList.remove('no-scroll');
+  menu.classList.remove('open');
+});
