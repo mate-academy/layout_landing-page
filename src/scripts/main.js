a75d6844b6486db61d66f75350112717a5e96657
vova <hohiop09@gmail.com>
2024-04-12 16:04:47 +0300

    some corrects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const menu = document.querySelector('.burger__logo');
+const body = document.querySelector('body');
+const navItem = document.querySelectorAll(".burger__nav-LIst-Item");
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  body.classList.toggle('overflow-hidden');
+
+  
+});
+
+navItem.forEach(elem => elem.addEventListener('click', () => {
+  menu.classList.remove('active');
+  body.classList.remove('overflow-hidden');
+}))
+
