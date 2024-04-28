e6a790b1c6d36edc124b33ffccb4e3f7893d9aad
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2024-04-28 10:12:04 +0100

    fixed problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const menuButton = document.querySelector('.icon--menu');
+
+menuButton.addEventListener('click', () => {
+  document.body.style.overflow = 'hidden';
+})
+
+
+const closeButton = document.querySelector('.icon--close');
+
+closeButton.addEventListener('click', () => {
+  document.body.style.overflow = '';
+})
