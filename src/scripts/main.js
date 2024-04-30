ccdda0dd826655b63403aaf071d0e417d3ea7370
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2024-04-30 11:20:18 +0100

    fixed scroll bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,18 @@ const menuButton = document.querySelector('.icon--menu');
 
 menuButton.addEventListener('click', () => {
   document.body.style.overflow = 'hidden';
-})
-
+});
 
 const closeButton = document.querySelector('.icon--close');
 
 closeButton.addEventListener('click', () => {
   document.body.style.overflow = '';
-})
+});
+
+const menuLinks = document.querySelectorAll('.nav__link');
+
+for (let i = 1; i <= 5; i++) {
+  menuLinks[i].addEventListener('click', () => {
+    document.body.style.overflow = '';
+  });
+}
