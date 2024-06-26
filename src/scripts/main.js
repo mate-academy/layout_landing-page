993cf8cecc77588a150d1ac37b788d03c0ad078c
Galers <galosaandrew@gmail.com>
2024-06-26 18:32:42 +0300

    Fixed height, fixed logic phone, disabled scroll and fixed height menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
 'use strict';
+// #region top-bar
+document.addEventListener('DOMContentLoaded', () => {
+  const phoneIcon = document.querySelector('.icon--phone');
+  const topBarPopup = document.querySelector('.top-bar__popup');
+
+  phoneIcon.addEventListener('click', () =>
+    topBarPopup.classList.toggle('popup--visible'),
+  );
+});
+// #endregion
+
+// #region preventDefault
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
+// #endregion
+
+// #region hidden scroll
+const menu = document.getElementById('menu');
+const showButton = document.getElementById('show');
+const closeButton = document.getElementById('close');
+
+const openMenu = () => {
+  menu.style.display = 'block';
+  document.body.classList.add('hide');
+};
+
+const closeMenu = () => {
+  menu.style.display = 'none';
+  document.body.classList.remove('hide');
+};
+
+showButton.addEventListener('click', () => openMenu());
+
+closeButton.addEventListener('click', () => closeMenu());
+// #endregion
