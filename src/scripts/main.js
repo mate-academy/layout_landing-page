66e81e203e7c49d5478aed11ee2a147304011ffd
vova <hohiop09@gmail.com>
2024-06-26 17:07:18 +0300

    corrected solution -fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,14 @@ const menu = document.querySelector('.burger__logo');
 const body = document.querySelector('body');
 const navItem = document.querySelectorAll(".burger__nav-LIst-Item");
 
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+});
+
 menu.addEventListener('click', () => {
+  console.log(1)
   menu.classList.toggle('active');
   body.classList.toggle('overflow-hidden');
-
-  
 });
 
 navItem.forEach(elem => elem.addEventListener('click', () => {
