56d8eeb268a2522c8bf669c077f08eaf5248e066
Katya <katya.stelmakhova@gmail.com>
2024-06-27 17:02:52 +0200

    Landing. Menu and height fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,39 @@
 const header = document.querySelector('.header');
 const burger = header.querySelector('.burger');
 const navMenu = header.querySelector('.nav');
+const navLinks = navMenu.querySelectorAll('.nav__link');
 const phoneBlock = header.querySelector('.header__contact');
+
+function menuToggler(burgerElement,navMenu,phoneIcon,header) {
+  burgerElement.classList.toggle('burger--active')
+  navMenu.classList.toggle('nav--active')
+  phoneIcon.classList.toggle('header__contact--active')
+  header.classList.toggle('header--active')
+
+  if (navMenu.classList.contains('nav--active'))
+    document.body.style.overflow = 'hidden';
+  else document.body.style.overflow = '';
+}
 
 function burgerMenuAction(burgerElement, navElement, phoneIcon, header) {
   burgerElement.addEventListener('click', () => {
-    burgerElement.classList.toggle('burger--active');
-    navElement.classList.toggle('nav--active');
-    phoneIcon.classList.toggle('header__contact--active');
-    header.classList.toggle('header--active');
-
-    if (navElement.classList.contains('nav--active'))
-      document.body.style.overflow = 'hidden';
-    else document.body.style.overflow = '';
+    menuToggler(burgerElement,navElement,phoneIcon,header)
   });
 }
 
-burgerMenuAction(burger, navMenu, phoneBlock, header);
+function closeMenuOnLinkClick(navLinks, navMenu, header, burgerElement, phoneIcon) {
+  for (let item of navLinks) {
+    item.addEventListener('click', () => {
+      menuToggler(burgerElement,navMenu,phoneIcon,header)
+    });
+  }
+}
+
+function headerScroll(header) {
+  window.addEventListener('scroll', () => {
+    window.scrollY > 50 ? header.classList.add('header--scroll') : header.classList.remove('header--scroll');
+  });
+}
 
 
 function observeElements(elementId) {
@@ -41,8 +58,12 @@ function observeElements(elementId) {
   });
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
   observeElements('observe-element');
+  headerScroll(header);
+  burgerMenuAction(burger, navMenu, phoneBlock, header);
+  closeMenuOnLinkClick(navLinks, navMenu, header, burger, phoneBlock);
 });
 
 
