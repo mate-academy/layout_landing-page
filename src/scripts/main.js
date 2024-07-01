7afe94d68a773e92085e6d766c4fee56ead8aef4
Roman Prykhodko <r.prykhodko@yahoo.com>
2024-07-01 09:43:25 +0300

    Created the landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+  var phoneIcon = document.querySelector('.header__nav-phone-img');
+
+  phoneIcon.addEventListener('click', function() {
+    var phoneNumber = document.querySelector('.header__nav-phone-number').textContent.trim();
+    window.location.href = 'tel:' + phoneNumber;
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuIcon = document.querySelector('.header__nav-menu-img');
+  const closeIcon = document.querySelector('.menu__close');
+  const menu = document.querySelector('.menu');
+
+  menuIcon.addEventListener('click', function() {
+    menu.style.display = 'block';
+  });
+
+  closeIcon.addEventListener('click', function() {
+    menu.style.display = 'none';
+  });
+});
