99241012d934cbb4792bf35c1d16704fb7ce20d2
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-07-08 00:48:58 +0200

    landing is ready to use

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+document.querySelector(".icon--menu").addEventListener('click', function() {
+  document.body.classList.add("scroll-disable");
+  console.log("hello");
+});
+
+document.querySelector(".icon--close").addEventListener('click', function() {
+  document.body.classList.remove("scroll-disable");
+});
+
+document.querySelectorAll(".nav__link").forEach((e) => {
+  e.addEventListener('click', function() {
+    document.body.classList.remove("scroll-disable");
+  })
+});
+
+
