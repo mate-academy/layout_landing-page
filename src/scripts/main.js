7acbd20aa2e3f59ea9ddba009765362dfc97661c
Maksym Mohyla <greenparksquad@gmail.com>
2024-07-18 20:07:59 +0300

    landing page complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const buttonGoTop = document.getElementById("buttonGoTop");
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    buttonGoTop.style.display = "block";
+  } else {
+    buttonGoTop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
