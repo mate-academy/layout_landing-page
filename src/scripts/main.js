198f10e7ae4a92a704de7f29714cdc66eadcc3b6
Teut`s <teuts@MacBook-Air.localdomain>
2024-07-20 14:21:05 +0300

    landingTask

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const burgerIcon = document.querySelector('.header__settings-burger');
+const crossIcon = document.querySelector('.header__settings-crossMenu');
+
+// const headerContainer = document.querySelector('.header__container');
+const menu = document.querySelector('.header__menu');
+
+burgerIcon.onclick = () => {
+  menu.style.display = 'block';
+};
+
+crossIcon.onclick = () => {
+  menu.style.display = 'none';
+};
