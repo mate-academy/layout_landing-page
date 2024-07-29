82a46f09a688386b6c5f5bd960656f42fb89e401
Teut`s <teuts@MacBook-Air.localdomain>
2024-07-29 18:21:17 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,8 @@
 
 const burgerIcon = document.querySelector('.header__settings-burger');
 const crossIcon = document.querySelector('.header__settings-crossMenu');
-
-// const headerContainer = document.querySelector('.header__container');
+const phoneIcon = document.querySelector('.header__settings-phone');
+const number = document.querySelector('.header__settings-number');
 const menu = document.querySelector('.header__menu');
 
 burgerIcon.onclick = () => {
@@ -12,4 +12,12 @@ burgerIcon.onclick = () => {
 
 crossIcon.onclick = () => {
   menu.style.display = 'none';
+};
+
+phoneIcon.onmouseover = () => {
+  number.style.display = 'block';
+};
+
+phoneIcon.onmouseout = () => {
+  number.style.display = 'none';
 };
