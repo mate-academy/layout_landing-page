632d693ad97b4e6f42261e924387cc337f45bfc0
Teut`s <teuts@MacBook-Air.localdomain>
2024-08-01 14:25:15 +0300

    fixFour

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,19 @@
 const burgerIcon = document.querySelector('.header__settings-burger');
 const crossIcon = document.querySelector('.header__settings-crossMenu');
 const phoneIcon = document.querySelector('.header__settings-phone');
+
 const number = document.querySelector('.header__settings-number');
 const menu = document.querySelector('.header__menu');
+const html = document.documentElement;
 
 burgerIcon.onclick = () => {
   menu.style.display = 'block';
+  html.classList.add('no-scroll');
 };
 
 crossIcon.onclick = () => {
   menu.style.display = 'none';
+  html.classList.remove('no-scroll');
 };
 
 phoneIcon.onmouseover = () => {
