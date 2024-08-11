9d9e3638680355cf1f274f5eb29d3f4c3d1c69a2
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-08-11 13:14:38 +0300

    add hover effects for img and fix bugs with menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const incon = document.querySelectorAll('.menu-toggle');
+incon.forEach((el) => {
+  el.addEventListener('click', () => {
+    document.body.classList.toggle('page__body--lock-scroll')
+  })
+})
