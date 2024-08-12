f2a209a8b5b67ce10a7cf5f929d6528ab1fe3086
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-08-12 16:34:05 +0300

    fix bugs 2

---
+++ dist/index.e817d5cb.js
@@ -0,0 +1,2 @@
+document.getElementById("menu");var o=document.querySelectorAll(".menu-toggle");"#menu"===window.location.hash&&document.body.classList.add("page__body--lock-scroll"),o.forEach(function(o){o.addEventListener("click",function(){document.body.classList.toggle("page__body--lock-scroll")})});
+//# sourceMappingURL=index.e817d5cb.js.map
