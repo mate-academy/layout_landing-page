f6d581f8ee92df76098102e5a000152cc99a7bf8
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-08-12 15:48:32 +0300

    fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const incon = document.querySelectorAll('.menu-toggle');
-incon.forEach((el) => {
+const menu = document.getElementById('menu');
+const menuToggle = document.querySelectorAll('.menu-toggle');
+
+if (window.location.hash === '#menu') {
+  document.body.classList.add('page__body--lock-scroll')
+}
+
+menuToggle.forEach((el) => {
   el.addEventListener('click', () => {
     document.body.classList.toggle('page__body--lock-scroll')
   })
