62ee5a736acbf968ec47b49f2dcd19cfc548403c
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-08-28 18:58:06 +0300

    fix header, menu and about blocks

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@
 
 const MENU_BUTTON = document.getElementById("menu-button");
 const MENU = document.getElementById("menu");
+const HEADER = document.querySelector("header");
+
+console.log(HEADER);
 
 const CALL_LINK = document.getElementById("call-link");
 
@@ -9,5 +12,6 @@ MENU_BUTTON.onclick = () => {
   CALL_LINK.classList.toggle("invisible");
   MENU_BUTTON.classList.toggle("active");
   MENU.classList.toggle("active");
+  document.querySelector('header').classList.toggle("header--bg-white");
   document.body.classList.toggle("unscrollable");
 };
