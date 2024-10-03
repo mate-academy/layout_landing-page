a60b53407eeb00d9493c8c34d2519aad640a7961
Artem Puzenko <a.puzenko2004@gmail.com>
2024-10-03 15:40:50 +0300

    fixed burger menu fonts and buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const phoneIcon = document.querySelector('.icon--phone');
   const phoneNumber = document.querySelector('.top-bar__phone-number');
-
-  phoneIcon.addEventListener('mouseenter', function () {
-    phoneNumber.classList.add('visible');
-  });
-
-  phoneIcon.addEventListener('mouseleave', function () {
-    phoneNumber.classList.remove('visible');
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  const phoneIcon = document.querySelector('.icon--phone');
-  const phoneNumber = document.querySelector('.top-bar__phone-number');
   const menu = document.getElementById('menu');
   const closeMenuIcon = document.querySelector('.icon--close');
+  // eslint-disable-next-line max-len
+  const logoLink = document.querySelector('.top-bar__logo-link--black');
 
   phoneIcon.addEventListener('mouseenter', function () {
     phoneNumber.classList.add('visible');
@@ -33,7 +22,24 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   closeMenuIcon.addEventListener('click', function () {
-    menu.style.right = '-1000%';
-    document.body.style.overflow = '';
+    closeMenu();
   });
+
+  const menuLinks = document.querySelectorAll('.nav__link');
+
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', function () {
+      closeMenu();
+    });
+  });
+
+  logoLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    closeMenu();
+  });
+
+  function closeMenu() {
+    menu.style.right = '-100%';
+    document.body.style.overflow = '';
+  }
 });
