d95c84ee592296a5376aaca032d7bb04df60d809
yuron-maker <komanitskiy200320@gmail.com>
2024-10-31 16:41:21 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,9 +9,11 @@ window.addEventListener('hashchange', () => {
 });
 
 const submitButton = document.querySelector('#id-submit');
+const form = document.querySelector('#id-Form');
 
 submitButton.addEventListener('click', submitButtonClick);
 
 function submitButtonClick(event) {
   event.preventDefault();
+  form.reset();
 }
