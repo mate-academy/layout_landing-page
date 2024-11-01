ce0fb18d03eb000cc1304c4bf868ff7ebd66fe64
RomanHasiuk <romanhasiuk1@gmail.com>
2024-11-01 22:36:32 +0200

    add hover number, convert text to links, set button type to submit, add favicon, fix header, and add footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+// const submitButton = document.querySelector('#id-submit');
+// const form = document.querySelector('#id-Form');
+
+// submitButton.addEventListener('click', submitButtonClick);
+// function submitButtonClick(event) {
+//   event.preventDefault();
+//   form.reset();
+// }
