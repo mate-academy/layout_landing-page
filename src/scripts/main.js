0d313009451d8b4e63d4003255126960c7127b14
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-11-04 19:01:01 +0100

    fix: correct images paths

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,44 @@
 'use strict';
+
+// function getMaximumProduct(nums) {
+//   const multiply = [];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       multiply.push(nums[i] * nums[j]);
+//     }
+//   }
+
+//   // eslint-disable-next-line no-console
+//   console.log(multiply);
+// }
+// getMaximumProduct([5, -4, 2, 8, 0]);
+
+// const charlie = { name: 'Charlie' };
+// // const joy = { name: 'Joy' };
+
+// charlie.partner = joy.name;
+// joy.partner = charlie.name;
+
+// // eslint-disable-next-line no-console
+// console.log(charlie);
+
+function upgradeRobot(robot, parts) {
+  const assignedObj = Object.assign(robot, parts);
+
+  // eslint-disable-next-line no-console
+  console.log(assignedObj.chipVer);
+}
+
+upgradeRobot(
+  {
+    chipVer: 9,
+    serialNo: 413,
+    wheels: 2,
+  },
+  [
+    { wheels: 6 },
+    { chipVer: 16 },
+    { displays: 2 },
+  ],
+);
