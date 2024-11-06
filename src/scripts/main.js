60b46ad45ab6a8d9279ac7bde6542cc6edd65ad4
ErasoumZe <zezinrique@gmail.com>
2024-11-06 18:33:09 -0300

    add modified Bose Landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+document.querySelectorAll('.nav__link').forEach((link) => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  });
+});
+
+const menu = document.querySelector('.page__menu');
+const page = document.querySelector('.page');
+
+menu.addEventListener('click', () => {
+  if (menu.classList.contains('active')) {
+    page.style.overflow = 'hidden';
+  } else {
+    page.style.overflow = 'auto';
+  }
+});
