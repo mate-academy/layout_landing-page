2bd8b1165212ad284608510622dbcfea6161a50a
Oleg Orykhivskiy <olegkoolll@gmail.com>
2024-11-07 21:09:54 +0200

    Test Switch-2

--- src/scripts/swithcer.js
+++ src/scripts/swithcer.js
@@ -2,13 +2,16 @@
 
 const page = document.documentElement;
 const switcher = document.querySelector('.theme-switcher');
+const aside = document.querySelector('.menu');
 
 switcher.addEventListener('click', () => {
   if (page.classList.contains('page--theme--dark')) {
+    aside.classList.remove('page--theme--dark');
     page.classList.remove('page--theme--dark');
     switcher.classList.remove('theme-switcher--theme--dark');
   } else {
     page.classList.add('page--theme--dark');
     switcher.classList.add('theme-switcher--theme--dark');
+    aside.classList.add('page--theme--dark');
   }
 });
