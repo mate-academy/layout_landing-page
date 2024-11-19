f4586283e677d48cfb2eafd1448a4654ae197ee3
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2024-11-19 12:41:20 +0200

    development process 0.16, add gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const burger = document.querySelector('.icon--burger');
+const iconClose = document.querySelector('.icon--close');
+const body = document.querySelector('.page__body');
+const pageMunu = document.querySelector('.page__menu');
+const navLinks = document.querySelectorAll('.nav__link');
+
+const burgerDataset = [burger, iconClose, ...navLinks];
+
+for (let i = 0; i < burgerDataset.length; i++) {
+  burgerDataset[i].addEventListener('click', () => {
+    pageMunu.classList.toggle('page__menu--active');
+    body.classList.toggle('page__body--lock');
+  });
+}
