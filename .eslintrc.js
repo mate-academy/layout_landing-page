1d0133a99d1c8e9f1c646c1c31c8c916a7d74f7d
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2024-12-25 09:30:48 -0300

    atualizando ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
+  extends: '@mate-academy/eslint-config',
+  env: {
+    browser: true,
+  },
+  plugins: ['html'],
+  overrides: [
+    {
+      files: ['*.html'],
+      parser: 'eslint-plugin-html',
+      rules: {
+        'html/indent': ['error', 2],
+      },
+    },
+  ],
 };
