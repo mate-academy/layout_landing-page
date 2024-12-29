41faf7cb939ecfedc4927a5ba50e12403e6f6cce
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2024-12-29 19:57:16 -0300

    add variables and mixin on header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
-  env: {
-    browser: true,
-  },
-  plugins: ['html'],
-  overrides: [
-    {
-      files: ['*.html'],
-      parser: 'eslint-plugin-html',
-      rules: {
-        'html/indent': ['error', 2],
-      },
-    },
-  ],
 };
