ad55be67722e4020929d15c97665bbd8ece17fb9
Gennadiy99 <login841@gmail.com>
2025-01-08 22:55:51 +0100

    change error list

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 const page = document.querySelector('.page__body');
 const linkBloсkScroll = document.querySelector('.icon--menu');
 const endBlockScroll = document.querySelector('.icon--close');
+const listMenu = document.querySelector('.nav__list');
 
 linkBloсkScroll.addEventListener('click', () => {
   page.classList.add('hidden');
@@ -10,4 +11,24 @@ linkBloсkScroll.addEventListener('click', () => {
 
 endBlockScroll.addEventListener('click', () => {
   page.classList.remove('hidden');
+});
+
+listMenu.addEventListener('click', () => {
+  page.classList.remove('hidden');
+});
+// остановка отправки формы на отсутствующий action="#"
+
+const form = document.querySelector('.contacts__form');
+const inputName = form.elements.contactName;
+const inputEmail = form.elements.contactEmail;
+const textArea = form.elements.contactMessage;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputName.value = '';
+  inputEmail.value = '';
+  textArea.value = '';
+
+  alert('Форма успешно отправленна!');
 });
