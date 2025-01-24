da79fd831aaadb053783ad1108ae94a4394e8657
Illia <hetsko.ilya@gmail.com>
2025-01-24 16:21:59 +0200

    fix landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const openMenuIcon = document.querySelector('.icon--menu');
+const closeMenuIcon = document.querySelector('.icon--close');
+
+// Проверяем, что элементы существуют
+if (openMenuIcon && closeMenuIcon) {
+  openMenuIcon.addEventListener('click', () => {
+    document.body.style.overflow = 'hidden';
+  });
+
+  closeMenuIcon.addEventListener('click', () => {
+    document.body.style.overflow = '';
+  });
+}
