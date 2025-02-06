189a69451315deb74c79d45bbbf23d7a3b90b39e
Misha <mizunskijm3@gmail.com>
2025-02-06 15:32:39 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-'use strict';
+const openMenuButton = document.querySelector('.icon--menu');
+const closeMenuButton = document.querySelector('.icon--close');
+const menu = document.getElementById('menu');
+const body = document.body;
+
+function openMenu() {
+  menu.classList.add('open');
+  body.classList.add('no-scroll');
+}
+
+function closeMenu() {
+  menu.classList.remove('open');
+  body.classList.remove('no-scroll');
+}
+
+openMenuButton.addEventListener('click', openMenu);
+
+closeMenuButton.addEventListener('click', closeMenu);
