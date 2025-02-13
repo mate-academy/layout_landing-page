ca808796b8d725e6b25c28458eb18fe73c3ece4c
Dmytro Kovtun <kovtundima2910@gmail.com>
2025-02-13 18:37:10 +0200

    add tast solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.page__btn');
+  const buttonMenu = document.querySelector('#button__menu');
+  const buttonClose = document.querySelector('#button_close');
+
+  buttonClose.addEventListener('click', () => {
+    document.body.style.overflow = '';
+  });
+
+  buttonMenu.addEventListener('click', () => {
+    document.body.style.overflow = 'hidden';
+  });
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      button.classList.toggle('pressed');
+    });
+  });
+});
