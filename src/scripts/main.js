3a4480dce6bd322c4a3892ec4e20b92b1ca8135c
Dmytro Kovtun <kovtundima2910@gmail.com>
2025-02-17 17:19:28 +0200

    added target _blank for link address and added hover for links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.page__btn');
   const buttonMenu = document.querySelector('#button__menu');
   const buttonClose = document.querySelector('#button_close');
+  const menuLinks = document.querySelectorAll('.nav__link');
 
   buttonClose.addEventListener('click', () => {
     document.body.style.overflow = '';
@@ -13,9 +14,22 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.style.overflow = 'hidden';
   });
 
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      document.body.style.overflow = '';
+    });
+  });
+
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       button.classList.toggle('pressed');
     });
   });
 });
+
+document
+  .getElementById('contact_form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+    this.reset();
+  });
