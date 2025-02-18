8c30d94797980e9e1688678d8d1998f5a2764deb
ItamarGouveia <contato.itamargouveia@gmail.com>
2025-02-18 13:54:21 -0300

    update requests

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.getElementById('menu');
+    const menuToggle = document.querySelector('.icon--menu');
+    const closeMenu = document.querySelector('.icon--close');
+    const body = document.body;
+  
+    // Quando o menu for aberto
+    menuToggle.addEventListener('click', function() {
+      menu.classList.add('menu--open');  // Se desejar animar o menu, adicione uma classe 'menu--open'
+      body.classList.add('no-scroll');   // Desativa a rolagem da página
+    });
+  
+    // Quando o menu for fechado
+    closeMenu.addEventListener('click', function() {
+      menu.classList.remove('menu--open');
+      body.classList.remove('no-scroll');  // Reabilita a rolagem da página
+    });
+  });
+  
