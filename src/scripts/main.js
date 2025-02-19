66ce6790eba998ae51633c7e0751ced6e0343af3
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-19 14:32:30 +0100

    disabled animations for safari and mozilla, disabled resize for textarea, delete button default paddings

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const isFirefox = navigator.userAgent.indexOf('Firefox') !== -1;
+const isSafari =
+  navigator.userAgent.indexOf('Safari') !== -1 &&
+  navigator.userAgent.indexOf('Chrome') === -1;
+
+if (isSafari || isFirefox) {
+  document.querySelector('.header').style.animation = 'none';
+  document.querySelector('.header').style.animationTimeline = 'none';
+  document.querySelector('.header').style.animationRange = 'none';
+}
