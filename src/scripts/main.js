05921c2b86cda3e23831923cb230725cd1dea35d
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-03-01 14:40:54 +0100

    Layout B&O

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const body = document.body;
+const header = document.querySelector('.header');
+const menu = document.querySelector('.menu');
+const menuOpen = document.querySelector('.top-bar__burger');
+const menuClose = document.querySelector('.menu__close');
+const navMenu = menu.querySelector('.nav-menu');
+const navMenuLinks = navMenu.querySelectorAll(
+  '.nav-menu__link',
+);
+
+menuOpen.addEventListener('click', () => {
+  menu.classList.add('menu--active');
+  body.classList.add('no-scroll');
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('menu--active');
+  body.classList.remove('no-scroll');
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.classList.add('header--scroll');
+  } else {
+    header.classList.remove('header--scroll');
+  }
+});
+
+navMenuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('menu--active');
+    body.classList.remove('no-scroll');
+  });
+});
