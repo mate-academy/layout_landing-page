3d21972f096cdf98268d1c1891357955f8700635
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-03-01 16:08:46 +0100

    fix menu logo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,7 @@ const menu = document.querySelector('.menu');
 const menuOpen = document.querySelector('.top-bar__burger');
 const menuClose = document.querySelector('.menu__close');
 const navMenu = menu.querySelector('.nav-menu');
+const menuTopBarLogo = menu.querySelector('.top-bar__logo');
 const navMenuLinks = navMenu.querySelectorAll(
   '.nav-menu__link',
 );
@@ -16,6 +17,11 @@ menuOpen.addEventListener('click', () => {
 });
 
 menuClose.addEventListener('click', () => {
+  menu.classList.remove('menu--active');
+  body.classList.remove('no-scroll');
+});
+
+menuTopBarLogo.addEventListener('click', () => {
   menu.classList.remove('menu--active');
   body.classList.remove('no-scroll');
 });
