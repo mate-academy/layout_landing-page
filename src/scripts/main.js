6ca9d9a928034a957d94f680499fab827e5d650a
anton <1288stef1288@gmail.com>
2025-03-05 17:36:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const html = document.querySelector('html');
+
+document.querySelector('.icon--burger').addEventListener('click', () => {
+  html.classList.toggle('unscroll');
+});
+
+document.querySelector('.nav__list').addEventListener('click', () => {
+  html.classList.toggle('unscroll');
+});
