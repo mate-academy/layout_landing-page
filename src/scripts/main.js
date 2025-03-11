f6fca7225a94ada3a2094c5409da7b284453782f
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-03-11 15:02:32 +0200

    refactoring code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Останавливаем стандартную отправку
+    alert("Форма отправлена!");
+  });
+});
