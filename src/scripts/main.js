4abadc78aad52c0832b4f99cd8dbc7a8694bc241
Yevheniia Biliaieva <biliaieva99@gmail.com>
2025-03-24 17:08:57 +0200

    fix all issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.icon--menu');
+const menuCloseButton = document.querySelector('.icon--close');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.add('menu--open');
+});
+
+menuCloseButton.addEventListener('click', () => {
+  menu.classList.remove('menu--open');
+});
