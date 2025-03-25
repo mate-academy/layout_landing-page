b9a56b6014c7ca55c224e5d5380622843d2df59b
Alessandro <Alessandrogabriel15160@gmail.com>
2025-03-25 19:44:05 -0300

    Repair telephone number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+// Exemplo com JavaScript comum
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  console.log({
+    name: event.target[0].value,
+    email: event.target[1].value,
+    message: event.target[2].value
+  });
+});
+
+
