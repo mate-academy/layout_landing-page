3ce6bd6507243e0f2177beeb5824edab6250ca76
Alessandro <Alessandrogabriel15160@gmail.com>
2025-03-26 10:14:21 -0300

    Create landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document
+  .querySelector('.main__contacts__form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const button = document.querySelector('.main__contacts__form__button');
+
+    button.textContent = 'Form submitted';
+    button.style.backgroundColor = 'green';
+  });
