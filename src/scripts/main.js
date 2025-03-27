eb1541bdb4fba2197300d5022298c81a04f46506
Nazar Bakay <bakayn484@gmail.com>
2025-03-27 12:52:56 +0200

    fixing 5 bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const page = document.querySelector('.page');
+
+document.querySelector('.icon--menu').addEventListener('click', () => {
+  page.classList.add('page--no-scroll');
+});
+
+document.querySelector('.icon--close').addEventListener('click', () => {
+  page.classList.remove('page--no-scroll');
+});
+
+const logoItems = document.querySelectorAll('.top-bar__logo-link');
+
+const secondLogoItem = logoItems[1];
+
+secondLogoItem.addEventListener('click', () => {
+  page.classList.remove('page--no-scroll');
+});
+
+const navItems = document.querySelectorAll('.nav__link');
+
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    page.classList.remove('page--no-scroll');
+  });
+});
