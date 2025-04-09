cd24cdd001ad38f4548f8056646985aed8ba86cf
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2025-04-08 20:03:16 -0700

    Add menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+const menuClose = document.getElementById('menu-close');
+const menuLinks = document.querySelectorAll('.menu__link');
+
+
+menuButton.addEventListener('click', () => {
+  menu.style.display = 'flex';
+});
+
+menuClose.addEventListener('click', () => {
+  menu.style.display = 'none';
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.style.display = 'none';
+  });
+});
