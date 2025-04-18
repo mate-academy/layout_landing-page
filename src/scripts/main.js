87f949d98b9ab32fcac240d68033d752cb90786d
Yaroslav <freefrip@gmail.com>
2025-04-18 12:36:25 +0300

    anim and favicon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const main = document.querySelector('.menu')
+const iconCloseMenu = document.querySelector('.icon--close')
+const iconOpenMenu = document.querySelector('.icon--menu')
+const navItems = document.querySelectorAll('.nav__item')
+const topBarLogo = document.querySelector('.top-bar__logo--aside')
+
+const menuOpeners = [iconOpenMenu, iconCloseMenu, topBarLogo];
+
+function eachOpen(query) {
+  query.forEach(item => {
+    item.addEventListener('click', () => {
+      main.classList.toggle('menu--active')
+    })
+  })
+}
+
+eachOpen(menuOpeners)
+eachOpen(navItems)
