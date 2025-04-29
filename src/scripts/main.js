2b1c5d40a9360115c7ab29df5d50f26ba544cf45
vtyacodes <vitiyah95@gmail.com>
2025-04-30 01:36:07 +0800

    submit landing page solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function menuClicked() {
+  const sideBar = document.querySelector('.sidebar');
+
+  sideBar.style.display = 'block';
+}
+
+function menuClosed() {
+  const sideBar = document.querySelector('.sidebar');
+
+  sideBar.style.display = 'none';
+}
+
+menuClicked();
+menuClosed();
