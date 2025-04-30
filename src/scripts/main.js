519f9ccc6ba0a260054c204bc2a94b7ced2ebb75
Viktor Kyshenchuk <viktor.kyshenchuk@gmail.com>
2025-04-30 22:42:37 +0300

    fix: mentor feedback

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const dropdown = document.querySelector('.top-bar__dropdown');
+const phone = document.querySelector('.top-bar__phone');
+
+phone.addEventListener('mouseover', () => {
+  dropdown.classList.add('top-bar__dropdown--active');
+});
+
+phone.addEventListener('mouseleave', () => {
+  dropdown.classList.remove('top-bar__dropdown--active');
+});
