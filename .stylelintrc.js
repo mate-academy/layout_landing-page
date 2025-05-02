340e24376ff912c5577088b6526b55e87aff1937
Yevhenii <evgen17ibragimov@gmail.com>
2025-05-02 17:13:00 +0200

    smooth page scrolling fixed

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: "@mate-academy/stylelint-config",
-  plugins: [
-    "stylelint-scss"
-  ],
-  rules: {}
+  extends: '@mate-academy/stylelint-config',
+  plugins: ['stylelint-scss'],
+  rules: {
+    'selector-max-id': 1,
+  },
 };
