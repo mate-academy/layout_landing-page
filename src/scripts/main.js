e502de4b8c655ae2844fadf0c71d873f5785200e
tobias <tobias.harasim@seznam.cz>
2025-05-02 13:11:06 +0200

    develoop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const hamburger = document.querySelector('.header__burger');
+const header = document.querySelector('.header');
+
+hamburger.addEventListener('click', () => {
+  if (!hamburger.classList.contains('active')) {
+    hamburger.classList.add('reset');
+
+    setTimeout(() => {
+      hamburger.classList.remove('reset');
+      hamburger.classList.add('active');
+      header.classList.add('active');
+    }, 400);
+  } else {
+    hamburger.classList.remove('active');
+    header.classList.remove('active');
+  }
+});
