5cc3eadde2839ea206d8956917befdfa0f33797a
Your Name <you@example.com>
2025-05-06 16:55:59 +0300

    met

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.icon--menu');
+  const close = document.querySelector('.icon--close');
+  const menu = document.getElementById('menu');
+
+  burger.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.add('menu--active');
+  });
+
+  close.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.remove('menu--active');
+  });
+});
