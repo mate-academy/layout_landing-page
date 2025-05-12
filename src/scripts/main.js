7883eac534b39f4439cdf9b32e9985ef8719fe64
annbisha <annskaylove@gmail.com>
2025-05-12 16:44:06 +0300

    trying to fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+// main.js
+document.addEventListener('DOMContentLoaded', () => {
+  const phoneIcon = document.querySelector('.header_right img[alt="Phone"]');
+  const contactsBlock = document.querySelector('.header_contacts');
+
+  phoneIcon.addEventListener('mouseenter', () => {
+    contactsBlock.style.display = 'flex';
+  });
+
+  phoneIcon.addEventListener('mouseleave', () => {
+    contactsBlock.style.display = 'none';
+  });
+
+  // Чтобы не пропадал, когда наводишь на сам блок
+  contactsBlock.addEventListener('mouseenter', () => {
+    contactsBlock.style.display = 'flex';
+  });
+
+  contactsBlock.addEventListener('mouseleave', () => {
+    contactsBlock.style.display = 'none';
+  });
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerIcon = document.querySelector('img[alt="Menu"]');
+  const menu = document.querySelector('.menu_nav');
+  const closeBtn = document.querySelector('.menu_close');
+
+  burgerIcon.addEventListener('mouseenter', () => {
+    menu.classList.remove('hidden');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    menu.classList.add('hidden');
+  });
+});
