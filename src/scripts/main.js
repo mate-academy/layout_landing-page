f17c037a17d160f83d2ae82eeeb15fad13cbb7d2
Petro Kustov <petr.kustov@skyrora.com>
2025-05-21 15:55:02 +0300

    menu interaction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function showMenu() {
+  document.getElementsByClassName('menu-wrapper')[0].style.visibility =
+    'visible';
+}
+
+function hideMenu() {
+  document.getElementsByClassName('menu-wrapper')[0].style.visibility =
+    'hidden';
+}
