6428f0dec3ddfe53c2404f1d0bed642f5afbcf40
A-Melnykk <969696245n@gmail.com>
2025-05-29 17:13:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.icon--burger');
+  const menu = document.getElementById('menu');
+  const close = document.querySelector('.icon--close');
+
+  if (burger && menu && close) {
+    burger.addEventListener('click', () => {
+      menu.classList.add('menu--active');
+    });
+
+    close.addEventListener('click', () => {
+      menu.classList.remove('menu--active');
+    });
+  } else {
+    // console.error('Burger or menu or close button not found!');
+  }
+});
