122193c9651e0c650791e83aedf51f7386e2ed79
Serhii Babskyi <sergbald99@gmail.com>
2025-06-15 22:14:37 +0300

    created contscts section, add button for categories, fix scroll with open menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+// block the scroll of the page when the menu is open
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
