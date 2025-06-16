31df15ba249f3899ec1b2be26075aa32e8fd5d0a
dopezinho <opugnero@gmail.com>
2025-06-16 18:58:57 -0300

    navbar working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
-'use strict';
+const hamburger = document.getElementById('hamburger');
+const closeNav = document.getElementById('close');
+
+hamburger.onclick.sidebar.style.left = '0';
+
+closeNav.onclick.sidebar.style.left = '-100%';
