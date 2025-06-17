21c1badc15a65fd89578fb1bed2fc8ea9e9aa9cd
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-06-16 22:33:25 -0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// menu mobile
+
+const menuBtn = document.querySelector('.mobile__btn');
+
+const menuClose = document.querySelector('.mobile__close');
+
+const menuMobile = document.querySelector('.menu__mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.add('showMobile');
+});
+
+menuClose.addEventListener('click', () => {
+  menuMobile.classList.remove('showMobile');
+});
+
+// show phone details
+
+const phoneBtn = document.querySelector('.header__phone');
+
+const phoneDetails = document.querySelector('.phone__details');
+
+phoneBtn.addEventListener('click', () => {
+  phoneDetails.classList.toggle('showDetails');
+});
