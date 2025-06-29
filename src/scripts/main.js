a12a98afce0b2695f3964abd6908c8a1d8d901b1
Aurimas <aurimasgedvilas08@gmail.com>
2025-06-29 22:18:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+import '../styles/modern-normalize.css';
+import '../styles/style.scss';
+import '../styles/components/hero.css';
+import '../styles/components/hours.css';
+import '../styles/components/view.css';
+import '../styles/components/more.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/menu.css';
+import '../styles/utils.css';
+import showMenu from '../utils/menu-nav.js';
+
+showMenu();
