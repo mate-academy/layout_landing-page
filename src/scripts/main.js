26676b1786e80e5135e99cd73443a8b443c43f90
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-01 17:41:09 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const body = document.querySelector('body');
+const phoneNumber = document.querySelector('.extra_nav__call');
+const burgerMenu = document.querySelector('.extra_nav__burger_menu');
+const nav = document.querySelector('nav');
+
+phoneNumber.addEventListener('click', () => {
+  phoneNumber.classList.toggle('extra_nav__call--active');
+});
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('extra_nav__burger_menu--active');
+
+  nav.classList.toggle('nav_open');
+
+  body.style.overflow = body.style.overflow === 'hidden' ? '' : 'hidden';
+});
