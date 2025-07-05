1d49673453450a30fb30f5174a116045dfdd29e2
Ana <anastasia.skripka.sao@gmail.com>
2025-07-05 12:03:56 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1500,
+  once: true,
+});
+
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
+});
