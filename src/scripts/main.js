1d6eb0d4090f68fa9e0766f110de391d523375cc
ByatesEduard <150972677+ByatesEduard@users.noreply.github.com>
2025-07-11 17:08:28 +0300

    fix task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const form = document.getElementById('myForm');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();  
+    form.reset();        
+    alert('Форма очищена!'); 
+  });
