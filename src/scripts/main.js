fd98f2eef8d214589b548ac1b6e61ead7488d88f
Renat <renatabdrah001@gmail.com>
2025-07-11 18:25:53 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.getElementById('submit-btn').addEventListener('click',
+  function (event) {
+
+    event.preventDefault();
+    document.getElementById("myForm").reset();
+  });
