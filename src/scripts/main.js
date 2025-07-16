ebbe97a0869bd25b8b252eb305330d74fe765c15
Lucas Mendes <164028753+14lucas-mendes@users.noreply.github.com>
2025-07-16 18:00:17 -0300

    ajustado menu e hover da imagens, incluido os links nos lugares correspondentes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.footer__form');
+  if (form) {
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      form.reset();
+    });
+  }
+});
