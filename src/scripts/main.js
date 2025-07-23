26999bbd65d561e6e9d9b7e5fbb3635cb3371e5d
Leorodrigs <leorodrigues.job@gmail.com>
2025-07-23 17:56:37 -0300

    fix: page should not reload when sending the form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.contact-us__form');
+
+  if (form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+    });
+  }
+});
