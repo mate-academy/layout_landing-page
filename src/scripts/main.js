ccdfdb74967085c4cd96869f009f17fbce0040c6
diego69-foda <diego.bkuhn@gmail.com>
2025-07-29 16:17:07 -0300

    mais algumas modificações

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.header__menu-toggle');
-    const menuClose = document.querySelector('.menu__close');
-    const menu = document.getElementById('main-menu');
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.header__menu-toggle');
+  const menuClose = document.querySelector('.menu__close');
+  const menu = document.getElementById('main-menu');
 
-    if (menuToggle && menuClose && menu) {
-        menuToggle.addEventListener('click', () => {
-            menu.classList.add('is-active');
-        });
+  if (menuToggle && menuClose && menu) {
+    menuToggle.addEventListener('click', () => {
+      menu.classList.add('is-active');
+    });
 
-        menuClose.addEventListener('click', () => {
-            menu.classList.remove('is-active');
-        });
-    }
+    menuClose.addEventListener('click', () => {
+      menu.classList.remove('is-active');
+    });
+  }
 });
