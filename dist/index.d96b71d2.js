fd3cbe5a36126ed1743b40347aa2413e193a35de
diego69-foda <diego.bkuhn@gmail.com>
2025-07-31 13:54:47 -0300

    agora com a pasta dist

---
+++ dist/index.d96b71d2.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".header__menu-toggle"),t=document.querySelector(".menu__close"),n=document.getElementById("main-menu");e&&t&&n&&(e.addEventListener("click",function(){n.classList.add("is-active")}),t.addEventListener("click",function(){n.classList.remove("is-active")}))});
+//# sourceMappingURL=index.d96b71d2.js.map
