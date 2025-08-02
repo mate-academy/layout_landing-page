c330127221be031ea675b4544e339e96f618e396
Valentyna <pr.valya1989@gmail.com>
2025-08-02 19:42:14 +0300

    added the main part of the layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const menuOpenBtn = document.querySelector('.icon--menu');
+const menuCloseBtn = document.querySelector('.icon--close');
+
+menuOpenBtn.addEventListener('click', () => {
+  document.body.classList.add('menu-open');
+});
+
+menuCloseBtn.addEventListener('click', () => {
+  document.body.classList.remove('menu-open');
+});
