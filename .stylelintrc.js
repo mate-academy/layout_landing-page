91f53dd315cb91e18d504ab3519ef81fb7ac09b7
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-08-11 23:45:12 +0300

    additional fixes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,11 +3,5 @@ module.exports = {
   plugins: [
     "stylelint-scss"
   ],
-  rules: {
-    "at-rule-empty-line-before": null,
-    "rule-empty-line-before": null,
-    "scss/operator-no-unspaced": [true, {
-      "ignore": ["/"]
-    }]
-  }
+  rules: {}
 };
