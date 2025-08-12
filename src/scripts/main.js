c5471d5c4017c10e3a1a7b3dd26f7d837d7811b1
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-08-12 18:48:50 +0300

    removed fixed footer image, set overflow hidden javascript code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const headerBurger = document.getElementById('header-burger');
+const menuClose = document.getElementById('menu-close');
+const body = document.getElementsByClassName('body');
+
+headerBurger.onclick(body.classList.add('menu--is-active'));
+menuClose.onclick(body.classList.remove('menu--is-active'));
