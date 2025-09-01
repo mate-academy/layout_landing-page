f6a62ba79f339f68f40e2d8880d422da27b06538
maksbtw <maks.20050817@gmail.com>
2025-09-01 14:42:58 +0200

    added header and menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-'use strict';
+// eslint-disable-next-line no-unused-vars
+const toggleMenu = () => {
+  document.getElementById('menu').classList.toggle('menu--opened');
+  document.getElementById('header').classList.toggle('header--moved');
+  document.body.classList.toggle('scroll-blocked');
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
