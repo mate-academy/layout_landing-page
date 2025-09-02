1f5175c9dfaf7ddc4c51099a59b0085786082fdc
Lucas Pioner Sartori <lucasartori2020@gmail.com>
2025-09-02 13:08:55 -0700

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('contactForm');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    return;
+  });
+});
