35a61c6b8a1f8d3c01ea7a8ebeec40693fc83df4
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-09-07 00:02:20 +0300

    Added changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,17 @@
 'use strict';
 
+let menuToggle = document.querySelectorAll('.header__menu-toggle')
+let body = document.querySelector('body')
+
+
+   menuToggle.forEach((itemToggle) => {
+        itemToggle.addEventListener('click', ()=> {
+            
+            if(body.classList.contains('_menu-open')) {
+                body.classList.remove('_menu-open')
+            } else {
+                body.classList.add('_menu-open')
+            }
+        })
+   })
+
