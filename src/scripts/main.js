a1a6eb276c8776fd325851233414a0f1c5f80586
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-09-08 17:00:06 +0300

    Fixed remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,28 @@
 'use strict';
 
 let menuToggle = document.querySelectorAll('.header__menu-toggle')
+let navItems = document.querySelectorAll('.nav__items')
+
 let body = document.querySelector('body')
 
 
-   menuToggle.forEach((itemToggle) => {
-        itemToggle.addEventListener('click', ()=> {
-            
-            if(body.classList.contains('_menu-open')) {
-                body.classList.remove('_menu-open')
-            } else {
-                body.classList.add('_menu-open')
-            }
-        })
-   })
+menuToggle.forEach((itemToggle) => {
+    itemToggle.addEventListener('click', ()=> {
+        
+        if(body.classList.contains('_menu-open')) {
+            body.classList.remove('_menu-open')
+        } else {
+            body.classList.add('_menu-open')
+        }
+    })
+})
+
+
+navItems.forEach((items) => {
+    items.addEventListener('click', ()=> {
+        
+        body.classList.remove('_menu-open')
+       
+    })
+})
 
