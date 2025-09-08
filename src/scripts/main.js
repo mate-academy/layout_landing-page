abd8a96761bd79c2c2a94bf7730a41eac9081ed3
Yana <marusina.yana@gmail.com>
2025-09-09 00:19:38 +0300

    already done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menuTrigger = document.querySelector('.icon--menu');
+const menuClose = document.querySelector('.menu__close');
+const bodyEl = document.body;
+
+menuTrigger.addEventListener('click', () => {
+  bodyEl.classList.add('menu-open');
+});
+
+menuClose.addEventListener('click', () => {
+  bodyEl.classList.remove('menu-open');
+});
