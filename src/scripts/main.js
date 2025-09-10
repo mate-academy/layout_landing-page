0fe83d4f7fae25efdb413442b5515870b3509a36
Yurii <j.zherebilov15@gmail.com>
2025-09-10 19:26:10 +0300

    Save local work before merge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function buildTrie(...words) {
+  const trie = {};
+
+  if (words.length === 0 || (words.length === 1 && !words[0])) {
+    return trie;
+  }
+
+  for (const word of words) {
+    let current = trie;
+    let prefix = '';
+
+    for (let i = 0; i < word.length; i++) {
+      prefix += word[i];
+
+      const isLastChar = i === word.length - 1;
+
+      if (!current[prefix]) {
+        current[prefix] = isLastChar ? null : {};
+      } else if (current[prefix] === null) {
+        current[prefix] = {};
+      }
+
+      if (!isLastChar) {
+        current = current[prefix];
+      }
+    }
+  }
+
+  return trie;
+}
+
+buildTrie('trie'); // {'t': {'tr': {'tri': {'trie': null}}}}
