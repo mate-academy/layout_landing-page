0021c30d59d0773da48343fcd839a252eb5cfec6
Ievgen Petryk <jakeshad123@gmail.com>
2025-09-21 21:13:28 +0300

    fixed wrong style, added actions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function (event) {
+  if (!form.checkValidity()) {
+    return;
+  }
+  event.preventDefault();
+
+  form.reset();
+});
