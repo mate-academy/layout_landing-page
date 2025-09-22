707078dfa4f2cef48162cd6972a5b9338d257ce2
ivandobychin <vanya8623vanya>
2025-09-22 20:07:22 +0300

    add landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const menuButtons = document.querySelectorAll('.trigger-nav--js');
+const menu = document.querySelector('.header__opennav-nav');
+
+menuButtons.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    menu.classList.toggle('header__opennav-nav--active');
+    document.body.classList.toggle('scroll-hidden');
+  });
+});
